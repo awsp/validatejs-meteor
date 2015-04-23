fd4a4c4a480aa61244f95634d407a063da6bb37a
awsp <anthonyspwu@gmail.com>
2015-04-22 15:05:48 -1000

    version 0.4.1, update library to 0.7.1

--- package.js
+++ package.js
@@ -1,31 +1,31 @@
 "use strict";
 
 Package.describe({
-  name: 'awsp:validatejs',
-  version: '0.4.0',
-  // Brief, one-line summary of the package.
-  summary: 'Meteor package for validate.js library. ',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/awsp/validatejs-meteor',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'awsp:validatejs',
+    version: '0.4.1',
+    // Brief, one-line summary of the package.
+    summary: 'Meteor package for validate.js library. ',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/awsp/validatejs-meteor',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use([
-    'momentjs:moment@2.10.0'
-  ]);
+Package.onUse(function (api) {
+    api.versionsFrom('1.0');
+    api.use([
+        'momentjs:moment@2.10.0'
+    ]);
 
-  api.addFiles([
-    'validatejs.js',
-    'bower_components/validate/validate.js'
-  ]);
+    api.addFiles([
+        'validatejs.js',
+        'bower_components/validate/validate.js'
+    ]);
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('awsp:validatejs');
-  api.addFiles('validatejs-tests.js');
+Package.onTest(function (api) {
+    api.use('tinytest');
+    api.use('awsp:validatejs');
+    api.addFiles('validatejs-tests.js');
 });
--- validatejs-tests.js
+++ validatejs-tests.js
@@ -1,10 +1,10 @@
 var self = this;
 
-Tinytest.add('Testing to see if validate library is loaded. ', function (test) {
+Tinytest.add('awsp:validatejs - Testing to see if validate library is loaded. ', function (test) {
   test.equal(typeof self.validate, "function");
 });
 
-Tinytest.add('ValidateJS library simple test run. ', function (test) {
+Tinytest.add('awsp:validatejs - ValidateJS library simple test run. ', function (test) {
   var result = self.validate({password: "bad"}, { password: { length: { minimum: 6 } } });
   test.notEqual(result, undefined);
 });
